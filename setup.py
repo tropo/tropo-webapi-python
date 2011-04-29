94c3cf661febdfcdd0cac19988239c849425662d
Dan York <dyork@Lodestar2.com>
2011-04-29 17:03:57 -0400

    Incremented version number to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name              = "tropo-webapi-python",
-      version           = "0.1.0",
+      version           = "0.1.1",
       url               = "http://github.com/tropo/tropo-webapi-python",
       maintainer        = "Voxeo Corp.",
       maintainer_email  = "support@tropo.com",
