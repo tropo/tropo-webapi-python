a9608d41e84de899d62fa16561c60f9b652e0ddc
egilchri <egilchri@gmail.com>
2011-05-31 07:12:57 -0400

    Tarball for 0.1.2 created

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name              = "tropo-webapi-python",
-      version           = "0.1.1",
+      version           = "0.1.2",
       url               = "http://github.com/tropo/tropo-webapi-python",
       maintainer        = "Voxeo Corp.",
       maintainer_email  = "support@tropo.com",
