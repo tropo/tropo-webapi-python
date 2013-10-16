2086ee65f9dcb87f8ef3d1287bc2791b8014597a
Kevin Bond <kbond@voxeolabs.com>
2013-10-16 16:54:43 -0400

    Added examples for these new methods in /test/test1.py

---
+++ test/test1.py
@@ -0,0 +1,34 @@
+import sys
+sys.path = ['..'] + sys.path
+from tropo import Choices, MachineDetection, JoinPrompt, LeavePrompt, On, Ask, Say, Tropo
+
+t = Tropo()
+
+#CPA
+mc = MachineDetection(introduction="THis is a CPA test", voice="Victor").json
+t.call("+14071234321", machineDetection=mc)
+
+#CPA with Boolean value which will detect CPA with 30 seconds of silence. 
+t.call("+14071234321", machineDetection=True)
+
+
+#Conference with join/leave prompts
+jp = JoinPrompt(value="Someone just joined the conference", voice="Victor").json
+lp = LeavePrompt(value="Someone just left the conference", voice="Victor").json
+t.conference(id="1234", joinPrompt=jp, leavePrompt=lp)
+
+
+whisper = {}
+
+c = Choices(value="1", mode="dtmf")
+ask = Ask(say="Press 1 to accept this call", choices=c).json
+whisper["ask"] = ask
+
+say = Say("You are now being connected to the call").json
+whisper["say"] = say
+
+t.transfer(to="+14071234321", on=whisper)
+t.on(event="incomplete", say="You are now being disconnected. Goodbye")
+
+print t.RenderJson()
+
