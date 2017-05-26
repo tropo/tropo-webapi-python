7f7a9a485c6cff10121b1af80b19376826263608
FrankYuRayoo <xiangjyu@cisco.com>
2017-05-26 11:24:17 +0800

    improvement/TROPO-11265-say

--- samples/gh-14.test_say.py
+++ samples/gh-14.test_say.py
@@ -8,18 +8,23 @@
 # Sample application using the itty-bitty python web framework from:
 #  http://github.com/toastdriven/itty
 
+import sys
+sys.path = ['..'] + sys.path
+
 from itty import *
 from tropo import Tropo, Session
 
 @post('/index.json')
 def index(request):
-    s = Session(request.body)
     t = Tropo()
-    t.say('12345', _as='DIGITS', voice='dave')
+    t.say('12345', _as='DIGITS', voice='dave', promptLogSecurity='suppress')
+    t.say('s s s s f f f ', promptLogSecurity='suppress')
+    t.say(["Hello, World", "How ya doing?"], promptLogSecurity = "suppredd")
     json = t.RenderJson()
     print json
+    print 'sys.path is %s' % sys.path
     return json
 
 
-run_itty()
+run_itty(server='wsgiref', host='192.168.26.1', port=8083)
 
